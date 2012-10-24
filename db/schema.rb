181388dcc24f16250dbf1e75ff3f9d2d55468103
freshlhy <freshlhy@gmail.com>
2012-10-24 17:07:08 +0800

    User work daily commit

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20121024104337) do
     t.boolean  "admin",           :default => false
   end
 
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
