91108100d20a183f88b5ac4395136ec602888dc9
huyu liu <735077491@qq.com>
2012-11-18 22:11:11 +0800

    采用CSS添加首页表格的tr hover事件

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,17 +1,1 @@
 //= require bootstrap
-$(function(){
-    $(".available tr").hover(function(){
-        this.className = 'available-hover';
-    },function(){
-        this.className = 'alert alert-success';
-    })
-    $(".occupied tr").hover(function(){
-        this.className = 'occupied-hover';
-    },function(){
-        this.className = 'alert alert-error';
-    })
-
-    $('a').focus(function () {
-        this.blur();
-    });
-})
--- db/schema.rb
+++ db/schema.rb
@@ -40,13 +40,6 @@ ActiveRecord::Schema.define(:version => 20121101015313) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "members", :force => true do |t|
-    t.string   "name"
-    t.integer  "group_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "trip_members", :force => true do |t|
     t.integer  "trip_id"
     t.integer  "member_id"
