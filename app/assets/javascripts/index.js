5c311e43b3c10d3b3e4f76fe7fb7460a2d931e60
liuhuyu <735077491@qq.com>
2012-11-19 09:00:26 +0800

    调整js结构

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,1 +1,8 @@
 //= require bootstrap
+jQuery(document).ready(function ($) {
+    $('table tbody tr').hover(function () {
+        $(this).addClass("tr-hover");
+    }, function () {
+        $(this).removeClass("tr-hover");
+    });
+});
