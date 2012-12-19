ed8113f8a9286be4e61b0238f77718d09ce4edfa
huyu liu <735077491@qq.com>
2012-12-19 22:36:49 +0800

    用户资料更新界面完成 包括修改密码

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,8 @@
 class UsersController < ApplicationController
 
-  before_filter :signed_in_user, only: [:edit, :update, :index, :new, :create, :destroy]
+  before_filter :signed_in_user, only: [:edit, :update]
   before_filter :correct_user, only: [:edit, :update]
-  before_filter :admin_user, only: [:new, :create, :destroy, :index]
 
-  def new
-    @user = User.new
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "用户已创建"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
 
   def edit
     @user = User.find(params[:id])
@@ -30,29 +11,14 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      flash[:success] = "资料已更新"
+      flash[:success] = "您的资料已更新"
       sign_in @user
-      redirect_to @user
+      redirect_to user_root
     else
       render 'edit'
     end
 
   end
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
-  def destroy
-
-    @user = User.find(params[:id])
-
-    #不允许管理员删除自己
-    redirect_to(root_path) if current_user?(@user)
-
-    @user.destroy
-    flash[:success] = "用户已删除."
-    redirect_to users_url
-  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
 
   validates :name, :presence => {:message => '用户名不能为空'}, Length: { maximum: 50 },
             :uniqueness => {:case_sensitive => false, :message => '用户重名'}
-  validates :password, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, :presence => {:message => '密码不能为空'}, Length: { minimum: 6, message: "密码长度最少6位"}
+  validates :password_confirmation,  :presence => true
 
   private
 
